2adf53dd40f0d6fbe58c71ddaa63751e017461d9
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-12-31 19:42:39 +0530

    update Home.tsx file

--- FRONTEND/e-chanelling/src/views/Home.tsx
+++ FRONTEND/e-chanelling/src/views/Home.tsx
@@ -1,9 +1,11 @@
 import Header from "../layouts/Header.tsx";
+import Hero from "../componenets/Hero/Hero.tsx";
 
 export default function Home() {
     return (
         <div className={""}>
             <Header />
+            <Hero />
         </div>
     );
 }
