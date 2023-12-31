3a17e0027dcea79b9b2014d6df448981cea0bac2
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2023-12-31 19:47:42 +0530

    add carousel to the project

---
+++ FRONTEND/e-chanelling/src/componenets/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import {CarouselHero} from "./HeroComponent/Carousel.Hero.tsx";
+
+export default function Hero(): JSX.Element {
+    return (
+        <>
+            <CarouselHero />
+        </>
+    );
+}
---
+++ FRONTEND/e-chanelling/src/componenets/Hero/HeroComponent/Carousel.Hero.tsx
@@ -0,0 +1,8 @@
+
+export function CarouselHero():JSX.Element{
+    return (
+        <div>
+
+        </div>
+    );
+}
