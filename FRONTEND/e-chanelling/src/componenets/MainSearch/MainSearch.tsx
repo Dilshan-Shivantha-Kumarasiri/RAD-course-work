3f5c0024871df96f45262932fee7eada2b0e5646
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-01-01 14:01:26 +0530

    main search context (done)

--- FRONTEND/e-chanelling/src/componenets/MainSearch/MainSearch.tsx
+++ FRONTEND/e-chanelling/src/componenets/MainSearch/MainSearch.tsx
@@ -5,22 +5,28 @@ import React from "react";
 export default function MainSearch(): JSX.Element {
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [bg , setBg] = React.useState("#00b5ec")
+    const [bg, setBg] = React.useState("#00b5ec")
+
     //bg prop is background color
 
-    function setColor(color:string): void{
+    function setColor(color: string): void {
         setBg(color)
     }
 
-    return(
+    return (
         <>
-            <section className={"responsivenesss"}>
-                <div className={"grid grid-cols-3"}>
-                    <SearchBtn main={"Doctor Channelling"} sub={"Channel a doctor"} bg="#00b5ec" handleClick={(color:string) =>setColor(color)} />
-                    <SearchBtn main={"Tele-Channelling"} sub={"Video consultation"} bg="#0057a4" handleClick={(color:string) => setColor(color)} />
-                    <SearchBtn main={"Driving License Medical"} sub={"Book an appointmen"} bg="#4cb847" handleClick={(color:string) =>setColor(color)} />
-                </div>
-                <SearchInputContext bg={bg} id={bg}/>
+            <section className={"main-search-function-content"}>
+                <section className={"responsivenesss main-search-function-content"}>
+                    <div className={"grid grid-cols-3"}>
+                        <SearchBtn main={"Doctor Channelling"} sub={"Channel a doctor"} bg="#00b5ec"
+                                   handleClick={(color: string) => setColor(color)}/>
+                        <SearchBtn main={"Tele-Channelling"} sub={"Video consultation"} bg="#0057a4"
+                                   handleClick={(color: string) => setColor(color)}/>
+                        <SearchBtn main={"Driving License Medical"} sub={"Book an appointmen"} bg="#4cb847"
+                                   handleClick={(color: string) => setColor(color)}/>
+                    </div>
+                    <SearchInputContext bg={bg} id={bg}/>
+                </section>
             </section>
         </>
     );
