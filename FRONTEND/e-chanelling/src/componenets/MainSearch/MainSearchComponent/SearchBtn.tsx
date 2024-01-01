1a1936ad5043db25263aae2c12fddbbd162d7d94
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-01-01 07:03:35 +0530

    change search context color

--- FRONTEND/e-chanelling/src/componenets/MainSearch/MainSearchComponent/SearchBtn.tsx
+++ FRONTEND/e-chanelling/src/componenets/MainSearch/MainSearchComponent/SearchBtn.tsx
@@ -11,7 +11,7 @@ export default function SearchBtn(props:Props): JSX.Element {
 
     return(
         <>
-            <div className={`-translate-y-16 h-20 bg-[${props.bg}] border flex`} style={style} onClick={()=>props.handleClick(props.bg)}>
+            <div className={`-translate-y-16 h-20 bg-[${props.bg}] border flex `} style={style} onClick={()=>props.handleClick(props.bg)}>
                 <p></p>
                 <p></p>
             </div>
