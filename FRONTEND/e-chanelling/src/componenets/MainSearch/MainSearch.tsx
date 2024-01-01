d25b3b3d8bbfe112474b2cf9341208f195d6f89c
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-01-01 05:38:34 +0530

    set up main function search

---
+++ FRONTEND/e-chanelling/src/componenets/MainSearch/MainSearch.tsx
@@ -0,0 +1,13 @@
+import SearchBtn from "./MainSearchComponent/SearchBtn.tsx";
+
+export default function MainSearch(): JSX.Element {
+    return(
+        <>
+            <section className={"responsivenesss"}>
+                <div>
+                    <SearchBtn />
+                </div>
+            </section>
+        </>
+    );
+}
--- FRONTEND/e-chanelling/src/views/Home.tsx
+++ FRONTEND/e-chanelling/src/views/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "../layouts/Header.tsx";
 import Hero from "../componenets/Hero/Hero.tsx";
+import MainSearch from "../componenets/MainSearch/MainSearch.tsx";
 
 export default function Home() {
     return (
         <div className={""}>
             <Header />
             <Hero />
+            <MainSearch />
         </div>
     );
 }
