31605718cb70fa09605f6fb8b06d86d20fa8f9cf
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-01-10 02:48:54 +0530

    customer reviews data

---
+++ FRONTEND/src/assets/data/review.data.ts
@@ -0,0 +1,23 @@
+export default  [
+    {
+        customerReview:"The convenience of accessing medical notes online was fantastic. Health Plus made managing my health records hassle-free! Best Healthcare services.",
+        customerName:"Sarah Johnson",
+        customerCountry:"Florida, USA",
+        reviewCount:0
+    },
+
+    {
+        customerReview:"As a busy professional, Health Plus has been a lifesaver. Quick prescriptions and refills without compromising on quality care. Thank you!",
+        customerName:"Michael Brown",
+        customerCountry:"New York, USA",
+        reviewCount:1
+    },
+
+    {
+        customerReview:"Health Plus transformed my healthcare experience! The online consultations were so convenient, and the doctors were knowledgeable and caring.",
+        customerName:"Esther Howard",
+        customerCountry:"Texas, USA",
+        reviewCount:2
+    }
+
+]
